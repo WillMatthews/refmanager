f741f6eebdf2b2058cabb6481f20d0d75d33c82b
WillMatthews <will.a.matthews@me.com>
2019-01-12 16:56:29 +0000

    made glaring errors in my PHP sorry

--- html/getpdf.php
+++ html/getpdf.php
@@ -1,7 +1,11 @@
-<h1> Obtaining your PDF... </h1>
 
 <?php
 $record=$_GET['record'];
+
+if(!$record){
+echo "<h1>No ID Data, Please Try Again.</h1>";
+exit();
+}
 
 include 'dbconn.php';
 
@@ -16,6 +20,15 @@ if(!$con) {
 $result=mysqli_query($con,$sql_SQRY);
 $row=mysqli_fetch_assoc($result);
 
+if(!$result){
+  echo "<h1>No Matching Record Found. Please Try Again</h1>";
+  exit();
+} elseif(!$row['pdf']) {
+  echo "<h1>No PDF Data Found. Please Try Again</h1>";
+} else {
+  echo "<h1>Obtaining your file...</h1>";
+}
+
 // DOWNLOAD FILE
 header('Content-Disposition: attachment; filename="'.$row['key'].'.pdf"');
 //header('Content-Type: application/pdf');// . _mime_content_type($_FILES['userFile']['name']));
@@ -25,6 +38,5 @@ header('Content-Length: ' . strlen($row['pdf']));
 header('Connection: close');
 
 echo stripslashes($row['pdf']);
-}
 
 ?>
