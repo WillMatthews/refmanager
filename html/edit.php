381ba7e610e96062b5afd3aff71d191538427739
Charles Matthews <31955202+CharlesMatthews@users.noreply.github.com>
2019-01-13 19:23:05 +0000

    Added Bootstrap + new record edit form

--- html/edit.php
+++ html/edit.php
@@ -7,9 +7,12 @@ body,td,th {
     font-size: medium;
 }
 </style>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
 </head>
 <body>
-
+<div class =container>
 <?php
 $record=$_GET['record'];
 
@@ -107,22 +110,81 @@ if ( !empty( $row["haspdf"] ) ) {
 <br/>
 
 <form action="edit.php" method="post">
-Key: <input type="text" name="key" value="<?php echo $key; ?>" ><br>
-Title: <input type="text" name="title" value="<?php echo $title; ?>" ><br>
-Author: <input type="text" name="author" value="<?php echo $author; ?>" ><br>
-Year: <input type="text" name="year" value="<?php echo $year; ?>" ><br>
+  <div class="form-row">
+    <div class="form-group col-md-3">
+      <label for="inputKey">Key</label>
+      <input type="text" class="form-control" name="key" placeholder="Key" value ="<?php echo $key; ?>">
+    </div>
+    <div class="form-group col-md-9">
+      <label for="inputTitle">Title</label>
+      <input type="text" class="form-control" name="title" placeholder="Title" value="<?php echo $title; ?>">
+    </div>
+  </div>
+  <div class="form-row">
+  <div class="form-group col-md-3">
+    <label for="inputYear">Year</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Year" value="<?php echo $year; ?>">
+  </div>
+  <div class="form-group col-md-9">
+    <label for="inputAuthor">Author</label>
+    <input type="text" class="form-control" name="author" placeholder="Test" value="<?php echo $author; ?>">
+  </div>
+ </div>
+<div class="form-group">
+	<label for="inputAbstract">Abstract</label>
+	<textarea class="form-control" name="abstract">"<?php echo $keywords; ?>"</textarea>
+
+</div>
+<div class="form-group">
+	<label for="inputComments">Comments</label>
+	<textarea class="form-control" name="comments">"<?php echo $comments; ?>"</textarea>
+
+</div>
+<div class="form-group">
+    <label for="inputURL">URL</label>
+    <input type="text" class="form-control" name="url" placeholder="URL..." value="<?php echo $url; ?>">
+</div>
+<div class="form-group">
+    <label for="inputAuthor">Keywords</label>
+    <input type="text" class="form-control" name="keywords" placeholder="Keywords..." value="<?php echo $keywords; ?>">
+</div>
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label for="inputVolume">Volume</label>
+      <input type="text" class="form-control" name="volume" value="<?php echo $volume; ?>">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputNumber">Number</label>
+      <input type="text" id="inputState" class="form-control" name="number" value="<?php echo $number; ?>">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputPages">Pages</label>
+      <input type="text" class="form-control" name="pages" value="<?php echo $pages; ?>">
+    </div>
+  </div>
+  <input type="hidden" value="<?php echo $record; ?>" name="id" />
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+<form action="edit.php" method="post">
+Key: <input type="text" name="key" value= ><br>
+Title: <input type="text" name="title" value= ><br>
+Author: <input type="text" name="author"  ><br>
+Year: <input type="text" name="year"  ><br>
 Abstract: <input type="text" name="abstract" value="<?php echo $abstract; ?>" ><br>
-Keywords: <input type="text" name="keywords" value="<?php echo $keywords; ?>" ><br>
-Volume: <input type="text" name="volume" value="<?php echo $volume; ?>" ><br>
-Number: <input type="text" name="number" value="<?php echo $number; ?>" ><br>
-Pages: <input type="text" name="pages" value="<?php echo $pages; ?>" ><br>
-URL: <input type="text" name="url" value="<?php echo $url; ?>" ><br>
+Keywords: <input type="text" name="keywords" value= ><br>
+Volume: <input type="text" name="volume" value= ><br>
+Number: <input type="text" name="number"  ><br>
+Pages: <input type="text" name="" value= ><br>
+URL: <input type="text" name="url" value="" ><br>
 Comments: <input type="text" name="comments" value="<?php echo $comments; ?>" ><br>
-<input type="hidden" value="<?php echo $record; ?>" name="id" />
+
 <input type="submit">
 </form>
 
 
-
+</div>
 </body>
 </html>
