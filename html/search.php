63388774f5c6b9dab924b3539b2d00e65474a0b0
Charles Matthews <31955202+CharlesMatthews@users.noreply.github.com>
2019-01-22 23:40:34 +0000

    Update search.php

--- html/search.php
+++ html/search.php
@@ -12,6 +12,10 @@ body,td,th {
     text-align: center;
     font-size: small;
 }
+mark, .mark {
+    padding: 0.2em;
+    background-color: #FFEE5A;
+}
 </style>
 -->
 <link rel="stylesheet" href="static/bootstrap.css">
